4279a81e0ec75423a09847153f2c84ed502f5b05
huaeew <huaeew@users.noreply.github.com>
2026-09-05 19:30:48 +0800

    fix native manifest linkage

--- test/windows-launcher.test.js
+++ test/windows-launcher.test.js
@@ -13,6 +13,7 @@ test('Windows launcher builds as a native x64 Win32 application', async () => {
   assert.match(project, /<PlatformToolset>v143<\/PlatformToolset>/);
   assert.match(project, /<RuntimeLibrary>MultiThreaded<\/RuntimeLibrary>/);
   assert.match(project, /<SubSystem>Windows<\/SubSystem>/);
+  assert.match(project, /<GenerateManifest>false<\/GenerateManifest>/);
   assert.doesNotMatch(project, /UseWPF|TargetFramework|SelfContained|PublishSingleFile/);
   assert.match(manifest, /PerMonitorV2/);
   assert.match(manifest, /requestedExecutionLevel level="asInvoker"/);
